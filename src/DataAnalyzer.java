d2a46c415586671ca09625b157efea68c9a4b625
Bastien56580 <bastien.lesueur56@gmail.com>
2022-10-17 17:32:39 +0200

    push1

---
+++ src/DataAnalyzer.java
@@ -0,0 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DataAnalyzer {
+
+    public static void main(String[] args) throws IOException {
+
+        DataSetReader dataSetReader= new DataSetReader();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nom du fichier à analyser svp :");
+        try{
+            String fichierValeur = scanner.nextLine();
+            dataSetReader.readFile("src/valeurs.txt");
+        } catch (FileNotFoundException | BadDataException e){
+            System.out.println(e.getMessage());
+            System.out.println("Ressaisir le nom du fichier");
+            String fichierValeur = scanner.nextLine();
+            dataSetReader.readFile("src/valeurs.txt");
+        }
+        
+
+
+
+
+
+
+
+
+
+
+        String chemin = new Scanner(System.in).nextLine();
+    }
+
+
+}
---
+++ src/DataSetReader.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DataSetReader {
+    private double[] data;
+
+
+    public double[] readFile(String filename) throws IOException {
+
+        Scanner sc = new Scanner(new File(filename));
+        this.readData(sc);
+
+        return data;
+    }
+
+    private void readData(Scanner sc) throws BadDataException {
+
+
+        if (sc.hasNextInt()) {
+            int longueur = sc.nextInt();
+            for (int i = 0; i < longueur; i++) {
+                readValue(sc, i);
+            }
+        } else {
+            throw new BadDataException("La première valeur n'est pas un int ou est null");
+        }
+
+    }
+
+    private void readValue(Scanner in, int i) throws BadDataException {
+        if (in.hasNextDouble()) {
+            this.data[i] = in.nextDouble();
+        } else {
+            throw new BadDataException("DatasetReader.readValue() : La donnée " + (i + 1) + " n'est pas un double");
+        }
+
+    }
+
+
+}
